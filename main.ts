2cdc9600ee372bd1ad5a94bbff9296610b847ca3
bremensen <matthieucollege13@gmail.com>
2024-10-18 16:05:47 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,6 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+})
 basic.forever(function () {
     led.plotBarGraph(
     input.soundLevel(),
     0
     )
+})
+basic.forever(function () {
+    basic.showString("Hello!")
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . . . .
+        `)
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.SmallHeart)
 })
